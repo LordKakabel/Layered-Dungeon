37e54a0837103ab7132283aff4a89d6e47303d93
Lord Kakabel <50675941+LordKakabel@users.noreply.github.com>
2022-02-06 22:52:48 -0500

    Update Credits.cs

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -1,6 +1,9 @@
 /*
  * Music: RPG Music Pack: Lite Edition II by Owl Theory Music
  * 
+ * Player Sprites: RE:CV Sprite v1.0 by DoubleLeggy https://www.deviantart.com/doubleleggy
+ * https://www.deviantart.com/doubleleggy/art/RE-CV-Sprites-v1-0-151003724 
  * 
+ * Eye Enemy Sprites: Zombies Lots Game by saranai.ru
  *
 */
